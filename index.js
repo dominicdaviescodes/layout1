c898ffc4871efea32793d22cedb52be487e78d19
dominicdaviescodes <codewithdom@gmail.com>
2020-12-22 21:59:49 +0000

    change text of elements by clicking button

--- index.js
+++ index.js
@@ -8,8 +8,25 @@ console.log(myButton);
 
 // make button do something
 
+// myButton.addEventListener('click', function () {
+// console.log('button was clicked');
+// change the background of the button to red
+// there is a better way using (e) the event.
+//   myButton.style.background = "red";
+// });
+
+// change h1 tag to say I'm learning js when btn is clicked
+// we  want id's when grabbing elements
+const headerText = document.getElementById('header-text');
+
+// myButton.addEventListener('click', function () {
+// headerText.innerHTML = 'I\'m learning JavaScript';
+// headerText.textContent = "I'm learning JavaScript";
+
+// });
+
+// make the button text equal the counter variable
+var counter = 456;
 myButton.addEventListener('click', function () {
-  // console.log('button was clicked');
-  // change the background of the button to red
-  myButton.style.background = "red";
+  myButton.textContent = counter;
 });
