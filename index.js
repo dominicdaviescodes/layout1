f697689b744c63b12c15e51e18e09fa56bbdd44a
dominicdaviescodes <codewithdom@gmail.com>
2020-12-22 21:42:54 +0000

    add starter files

---
+++ index.js
@@ -0,0 +1,7 @@
+// use the document to grab the button. Store it in a JavaScript variable, and then log it out!
+// var clickMe = document.getElementById('clickMe');
+// console.log(clickMe);
+
+// better naming
+var myButton = document.getElementById('btn');
+console.log(myButton);
